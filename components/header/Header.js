b90ebd81b4fe8c22c0da7abd86b1efa793fbfed4
julianmed <julian.medina7@hotmail.com.ar>
2021-09-27 13:15:49 -0300

    finish with the header icons and adding styles and minimals changes

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,19 +1,24 @@
 import { 
     HomeIcon,
     BadgeCheckIcon,
+    LightningBoltIcon,
     CollectionIcon,
-    LibraryIcon,
     SearchIcon,
-    User
+    UserIcon
 } from '@heroicons/react/outline';
 import Image from 'next/image';
 import { HeaderIcons } from './HeaderIcons';
 
 export const Header = () => {
     return (
-        <header>
-            <ul>
+        <header className="flex flex-col sm:flex-row m-5 sm:justify-between items-center">
+            <ul className="flex flex-grow justify-evenly max-w-2xl">
                 <HeaderIcons title="HOME" Icon={HomeIcon}/>
+                <HeaderIcons title="TRENDING" Icon={LightningBoltIcon}/>
+                <HeaderIcons title="VERIFIED" Icon={BadgeCheckIcon}/>
+                <HeaderIcons title="COLLECTIONS" Icon={CollectionIcon}/>
+                <HeaderIcons title="SEARCH" Icon={SearchIcon}/>
+                <HeaderIcons title="ACCOUNT" Icon={UserIcon}/>
             </ul>
             <Image
                 className="object-contain"
--- components/header/HeaderIcons.js
+++ components/header/HeaderIcons.js
@@ -1,7 +1,7 @@
 export const HeaderIcons = ({title, Icon}) => {
     return (
-        <li className="flex flex-col items-center cursor-pointer group">
-            <Icon className="h-8 m-1"/>
+        <li className="hover:text-white flex flex-col items-center cursor-pointer group w-12 sm:w-20">
+            <Icon className="h-8 m-1 group-hover:animate-bounce"/>
             <p className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</p>
         </li>
     )
